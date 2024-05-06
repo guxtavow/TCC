58866e9afa58164de181376f47141a397fe9efd3
guxtavow <aquinoavilag@gmail.com>
2024-05-06 17:05:33 -0300

    feita a primeira versão da pagina de sobre

--- TCC/src/App.js
+++ TCC/src/App.js
@@ -2,13 +2,14 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Header from './componentes/navbar/Header';
 import Footer from './componentes/footer/Footer';
+import Sobre from './componentes/Sobre/Sobre';
 
 
 function App() {
   return (
     <div className="App">
       <Header />
-      
+      <Sobre />
       <Footer />
     </div>
   );
