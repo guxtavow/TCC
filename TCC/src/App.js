f8270303ffe70a1258b319d35c702235162dcafe
guxtavow <aquinoavilag@gmail.com>
2024-08-21 14:00:24 -0300

    fix

--- TCC/src/App.js
+++ TCC/src/App.js
@@ -4,13 +4,15 @@ import Header from './componentes/navbar/Header';
 import Footer from './componentes/footer/Footer';
 import Sobre from './componentes/Sobre/Sobre';
 import Chatbot from './componentes/Chatbot/chat';
+import Home from './componentes/Home/Home';
+import LoginScreen from './componentes/LoginScreen/LoginScreen';
 
 
 function App() {
   return (
     <div className="App">
       <Header />
-        <Chatbot/>
+        <Chatbot />
       <Footer />
     </div>
   );
