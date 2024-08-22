e9796fca0e90a0ff893c9efd4032731edc080281
guxtavow <aquinoavilag@gmail.com>
2024-08-22 11:27:21 -0300

    Adicionado o resto da pagina do chatbot, além de pequenos ajustes e também comentários no código.

--- TCC/src/componentes/Chatbot/chat.js
+++ TCC/src/componentes/Chatbot/chat.js
@@ -1,6 +1,7 @@
 import './chat.css'
 import Robo from './robo.png'
 import Lampada from './lampada.png'
+import Robochat from './robozinho.png'
 
 
 export default function Chatbot(){
@@ -9,7 +10,7 @@ export default function Chatbot(){
 
     return(
         <>
-            <div className='Chat'>
+            <div className='Chat'> {/* PAGINA DO CHATBOT NO GERAL */}
                 <div className='TituloChat'>
                     <img src={Robo} width={50} 
                     style={{
@@ -24,6 +25,17 @@ export default function Chatbot(){
                     alt='ideia' id='lampada-title'/>
                     <br />
                         <span id='titleP2'>Seu assistente tecnólogico para aprender brincando!</span>
+                </div>
+
+                <div className='Chatbot'>{/* ROBOZINHO COM CHATBOT NA BARRIGA */}
+                    <img src={Robochat} alt='robochat' id='robochat' width={1400} /> {/* FUNDO CHAT */}
+                    <div className='TextoChat'>
+                        <div className='sobreChat'>{/* INFOS SOBRE O CHAT */}
+                            <h2 id='titleChat'>Olá! Eu sou o TechBot!</h2>
+                            <p id='textChat'>Seu assistente virtual especializado em tecnologia! Estou aqui para ajudar você a aprender sobre tecnologia e programação de uma forma divertida e interativa. 
+                                Vamos explorar o mundo da tecnologia juntos?</p>
+                        </div>
+                    </div>
                 </div>
             </div>
     
