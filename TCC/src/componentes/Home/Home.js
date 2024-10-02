f2863a693d3084d822f3f8802bd42fda8e9ddf4b
ArielBPeixoto13 <ariel.peixoto@gerdau.com.br>
2024-10-02 14:10:40 -0300

    Home - Update - 14:10

--- TCC/src/componentes/Home/Home.js
+++ TCC/src/componentes/Home/Home.js
@@ -2,42 +2,75 @@ import React from 'react';
 import './Home.css';
 
 import imagem1 from './elements/imagem1.png';
+import btnCadastro from './elements/btn-cad.png';
+import btnEntrar from './elements/btn-entrar.png';
+import imagem2 from './elements/imagem2.png';
+
 
 export default function Home() {
   return (
     <div className="container">
       <main className="home-content">
-
-        {/* Conteúdo da seção 1 */}
+        
+        {/* Seção 1 */}
         <div className="section-content section1">
           <div className="text-content">
             <h3>Conexão de Conhecimento:</h3>
             <h3>Explorando a Tecnologia de Forma Divertida</h3>
             <p>
-              Bem-vindo ao <strong>TechKids</strong>, seu assistente de estudos virtual! 
-              Aqui, aprender é divertido, e juntos iremos descobrir o fascinante mundo da tecnologia. 
+              Bem-vindo ao TechKids, seu assistente de estudos virtual! 
+              Aqui, aprender é divertido, e juntos iremos descobrir o fascinante mundo da tecnologia.
             </p>
           </div>
           <div className="image-content">
-            <img src={imagem1} alt="Exemplo Seção 1" />
+            <img src={imagem1} alt="IA" />
           </div>
         </div>
 
-        {/* Conteúdo da seção 2 */}
+        {/* Seção 2 */}
         <div className="section-content section2">
-          {/* Adicione conteúdo aqui */}
+          <div className="text-content">
+            <p>Mas antes...</p>
+            <p><strong>
+              Acesse ou crie seu cadastro para acessar um conteúdo personalizado de acordo com o seu perfil:
+            </strong></p>
+            <div className="button-container">
+              <button className="action-button">
+                <img src={btnCadastro} alt="Botão Cadastro" className="button-icon" />
+                <strong>Cadastro</strong>
+              </button>
+              <button className="action-button">
+                <img src={btnEntrar} alt="Botão Entrar" className="button-icon" />
+                <strong>Entrar</strong>
+              </button>
+            </div>
+            <div className="speech-bubble">
+              <h3><strong>
+                Somos o TECHKIDS, <br/>
+                um time apaixonado por tecnologia.
+              </strong></h3>
+              <p>
+                Conheça o TechBot, nosso amigo virtual que responde suas dúvidas e 
+                te guia no mundo digital. Além disso, temos jogos incríveis para tornar o 
+                aprendizado ainda mais divertido!
+                Quer saber mais? <a href="#">Clique aqui</a> para descobrir tudo sobre nós!
+              </p>
+            </div>
+            <div className="image-below-bubble">
+             <img src={imagem2} alt="Imagem TechBot" />
+          </div>
+          </div>
         </div>
 
-        {/* Conteúdo da seção 3 */}
+        {/* Seção 3 */}
         <div className="section-content section3">
           {/* Adicione conteúdo aqui */}
         </div>
 
-        {/* Conteúdo da seção 4 */}
+        {/* Seção 4 */}
         <div className="section-content section4">
           {/* Adicione conteúdo aqui */}
         </div>
-
       </main>
     </div>
   );
