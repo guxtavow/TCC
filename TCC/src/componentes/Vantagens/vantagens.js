813f4aad36bd86926d731727b0ad26e2dce34789
guxtavow <aquinoavilag@gmail.com>
2024-10-03 21:28:29 -0300

    Fix: arrumado front do figma

---
+++ TCC/src/componentes/Vantagens/vantagens.js
@@ -0,0 +1,12 @@
+import comoFunciona from './como.png'
+export default function Vantagens(){
+
+
+return(
+    <div className="vantagem">
+        <div className="Banner">
+            <img src={comoFunciona} alt='como-funciona' id='ComoFunciona'/>
+        </div>
+    </div>
+    )
+}
