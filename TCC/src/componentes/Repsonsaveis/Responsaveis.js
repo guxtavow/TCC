3db4986a5ea3d5900833de6331897a7bf8894cf4
ArielBPeixoto13 <ariel.peixoto@gerdau.com.br>
2024-10-04 11:03:28 -0300

    Update - 04.10.24 - 11h

--- TCC/src/componentes/Repsonsaveis/Responsaveis.js
+++ TCC/src/componentes/Repsonsaveis/Responsaveis.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import './Responsaveis.css';
 
+import leftImage from './elements/imagem1.png';
+import rightImage from './elements/imagem2.png';
+
+
+
 export default function Responsaveis(){
 return (
-<>
- <p>TESTE</p>
-</>
+<div className="performance-indicator">
+      <div className="header-content">
+        <img src={leftImage} alt="Left" className="left-image" />
+        <h3>Indicador de Desempenho</h3>
+        <img src={rightImage} alt="Right" className="right-image" />
+      </div>
+      <p>
+        Acompanhe o desempenho do seu filho(a)/aluno de maneira intuitiva e dinamica! <br/>
+        Aqui voce irá acompanhar o perfil do filho(a)/aluno e seu desenvolvimento utilizando nossa plataforma.<br/>
+        E também poderá editar preferencias do perfil.
+      </p>
+    </div>
 )
 }
 
