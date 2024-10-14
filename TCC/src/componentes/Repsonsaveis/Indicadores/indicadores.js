d642eaff5c62e8607922541d12bd87da0e0c4284
ArielBPeixoto13 <ariel.peixoto@gerdau.com.br>
2024-10-14 14:16:50 -0300

    Responsavais update - 14.10.2024 - 14h

--- TCC/src/componentes/Repsonsaveis/Indicadores/indicadores.js
+++ TCC/src/componentes/Repsonsaveis/Indicadores/indicadores.js
@@ -7,8 +7,8 @@ import concluidosIcon from './elements/concluidos.png';
 import desempenhoIcon from './elements/desempenho.png';
 
 // Componente Card
-const Card = ({ title, value, linkText, linkUrl, icon }) => (
-  <div className="indicator-card"> {/* Mova a classe aqui para o card */}
+const Card = ({ title, value, linkText, linkUrl, icon, className }) => (
+  <div className={`indicator-card ${className}`}> {/* A classe agora inclui a className */}
     <div className="card-details">
       <h3>{title}</h3>
       <p>{value}</p>
@@ -20,11 +20,11 @@ const Card = ({ title, value, linkText, linkUrl, icon }) => (
 
 export default function Indicators() {
   const indicatorsData = [
-    { title: "Quantidades de Atividades", value: "50", linkText: "Ver Atividades", linkUrl: "#", icon: atividadesIcon },
-    { title: "Atividades Concluídas", value: "25", linkText: "Ver Atividades", linkUrl: "#", icon: concluidosIcon },
-    { title: "Desempenho", value: "50%", linkText: "Ver Desempenho", linkUrl: "#", icon: desempenhoIcon },
+    { title: "Quantidades de Atividades", value: "50", linkText: "Ver Atividades", linkUrl: "#", icon: atividadesIcon, className: "card-1" },
+    { title: "Atividades Concluídas", value: "25", linkText: "Ver Atividades", linkUrl: "#", icon: concluidosIcon, className: "card-2" },
+    { title: "Desempenho", value: "50%", linkText: "Ver Desempenho", linkUrl: "#", icon: desempenhoIcon, className: "card-3" },
   ];
-
+  
   return (
     <div className="indicators-page">
       <h2>Indicadores de Desempenho</h2>
@@ -37,6 +37,7 @@ export default function Indicators() {
             linkText={indicator.linkText}
             linkUrl={indicator.linkUrl}
             icon={indicator.icon}
+            className={indicator.className} // Adicionada a className
           />
         ))}
       </div>
