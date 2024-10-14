50525fb1652ae004d1e58c2866ac04ad009b6916
ArielBPeixoto13 <ariel.peixoto@gerdau.com.br>
2024-10-14 13:58:35 -0300

    Responsavais update - 14.10.2024 - 14h

--- TCC/src/componentes/Repsonsaveis/Indicadores/indicadores.js
+++ TCC/src/componentes/Repsonsaveis/Indicadores/indicadores.js
@@ -8,21 +8,21 @@ import desempenhoIcon from './elements/desempenho.png';
 
 // Componente Card
 const Card = ({ title, value, linkText, linkUrl, icon }) => (
-  <div className="card">
+  <div className="indicator-card"> {/* Mova a classe aqui para o card */}
     <div className="card-details">
       <h3>{title}</h3>
       <p>{value}</p>
       <a href={linkUrl}>{linkText}</a>
     </div>
-    <img src={icon} alt={`${title} icon`} className="card-icon" /> {/* Adiciona a classe card-icon */}
+    <img src={icon} alt={`${title} icon`} className="card-icon" /> {/* Imagem à direita */}
   </div>
 );
 
 export default function Indicators() {
   const indicatorsData = [
-    { title: "Quantidades de Atividades", value: "50", linkText: "Ver Atividades", linkUrl: "#", icon: atividadesIcon, className: "card-1" },
-    { title: "Atividades Concluídas", value: "25", linkText: "Ver Atividades", linkUrl: "#", icon: concluidosIcon, className: "card-2" },
-    { title: "Desempenho", value: "50%", linkText: "Ver Desempenho", linkUrl: "#", icon: desempenhoIcon, className: "card-3" },
+    { title: "Quantidades de Atividades", value: "50", linkText: "Ver Atividades", linkUrl: "#", icon: atividadesIcon },
+    { title: "Atividades Concluídas", value: "25", linkText: "Ver Atividades", linkUrl: "#", icon: concluidosIcon },
+    { title: "Desempenho", value: "50%", linkText: "Ver Desempenho", linkUrl: "#", icon: desempenhoIcon },
   ];
 
   return (
@@ -30,15 +30,14 @@ export default function Indicators() {
       <h2>Indicadores de Desempenho</h2>
       <div className="indicators-container">
         {indicatorsData.map((indicator, index) => (
-          <div className={`indicator-card ${indicator.className}`} key={index}>
-            <Card
-              title={indicator.title}
-              value={indicator.value}
-              linkText={indicator.linkText}
-              linkUrl={indicator.linkUrl}
-              icon={indicator.icon}
-            />
-          </div>
+          <Card
+            key={index}
+            title={indicator.title}
+            value={indicator.value}
+            linkText={indicator.linkText}
+            linkUrl={indicator.linkUrl}
+            icon={indicator.icon}
+          />
         ))}
       </div>
     </div>
