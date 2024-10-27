ffab6b7df3d18f802938db404162d89bf3ee43ef
ArielBPeixoto13 <ariel.peixoto@gerdau.com.br>
2024-10-27 12:40:19 -0300

    Update - 12:40h

--- TCC/src/componentes/Repsonsaveis/Calendar/Calendar.js
+++ TCC/src/componentes/Repsonsaveis/Calendar/Calendar.js
@@ -1,46 +1,15 @@
-// Calendar.js
-import React, { useState } from "react";
+import React from "react";
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
-import interactionPlugin from "@fullcalendar/interaction";
-
-
 import './Calendar.css'; // Importa o CSS do calendário
 
 const Calendar = () => {
-  const [currentEvents, setCurrentEvents] = useState([]);
-
-  const handleDateClick = (selected) => {
-    const title = prompt("Please enter a new title for your event");
-    const calendarApi = selected.view.calendar;
-    calendarApi.unselect(); // Desmarcar o dia
-
-    if (title) {
-      calendarApi.addEvent({
-        id: `${selected.dateStr}-${title}`,
-        title,
-        start: selected.startStr,
-        end: selected.endStr,
-        allDay: selected.allDay,
-      });
-    }
-  };
-
-  const handleEventClick = (selected) => {
-    if (window.confirm(`Are you sure you want to delete the event '${selected.event.title}'`)) {
-      selected.event.remove();
-    }
-  };
-
   return (
     <FullCalendar
-      plugins={[dayGridPlugin, interactionPlugin]}
+      plugins={[dayGridPlugin]}
       initialView="dayGridMonth"
-      editable={true}
-      selectable={true}
-      select={handleDateClick}
-      eventClick={handleEventClick}
-      eventsSet={(events) => setCurrentEvents(events)} // Atualiza os eventos atuais
+      editable={false} // Desabilita a edição
+      selectable={false} // Desabilita a seleção de dias
     />
   );
 };
