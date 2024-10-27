0f74881c9838e17a26d1d994975e0a4cf6b0d7de
ArielBPeixoto13 <ariel.peixoto@gerdau.com.br>
2024-10-27 10:12:13 -0300

    Update 27.10.24 -- 10h

--- TCC/src/componentes/Repsonsaveis/Responsaveis.js
+++ TCC/src/componentes/Repsonsaveis/Responsaveis.js
@@ -3,29 +3,9 @@ import './Responsaveis.css';
 import leftImage from './elements/imagem1.png';
 import rightImage from './elements/imagem2.png';
 
-import quantidadesIcon from './elements/quantidades.png';
-import concluidosIcon from './elements/concluidos.png';
-import desempenhoIcon from './elements/desempenho.png';
-
-
-function Card({ title, value, linkText, icon, className }) {
-  return (
-<div className={`indicator-card ${className}`}>
-      <div className="card-content">  {/* Wrapper flex para texto e imagem */}
-        <div className="card-details">
-          <h3>{title}</h3>
-          <p>{value}</p>
-          {linkText && <a href="#">{linkText}</a>}
-        </div>
-        {icon && <img src={icon} alt={`${title} icon`} className="card-icon" />} {/* Ícone à direita */}
-      </div>
-    </div>
-  );
-}
-
 export default function Responsaveis() {
   return (
-    <>
+    <div className="responsible-container">
       <div className="performance-indicator">
         <div className="header-content">
           <img src={leftImage} alt="Left" className="left-image" />
@@ -39,32 +19,47 @@ export default function Responsaveis() {
         </p>
       </div>
 
-      {/* Conteúdo dos Indicadores */}
-      <div className="indicators-page">
-      <div className="first-row">
-        <Card title="Quantidades de Atividades" value="50" linkText="Ver Atividades" icon={quantidadesIcon} className="card-1" />
-        <Card title="Atividades Concluídas" value="25" linkText="Ver Atividades" icon={concluidosIcon} className="card-2" />
-        <Card title="Desempenho" value="50%" linkText="Ver Desempenho" icon={desempenhoIcon} className="card-3" />
+      <div className="dashboard">
+        <div className="grid-container">
+          <div className="indicator-card card-1">
+            <h3>Atividades</h3>
+            <p>12</p>
+            <a href="#">Ver mais</a>
+          </div>
+          <div className="indicator-card card-2">
+            <h3>Concluídas</h3>
+            <p>8</p>
+            <a href="#">Ver mais</a>
+          </div>
+          <div className="indicator-card card-3">
+            <h3>Desempenho</h3>
+            <p>85%</p>
+            <a href="#">Ver mais</a>
+          </div>
+
+          <div className="indicator-card card-4">
+            <h3>Perguntas Respondidas</h3>
+            <p>150</p>
+            <a href="#">Ver mais</a>
+          </div>
+          <div className="indicator-card card-5">
+            <h3>Calendário</h3>
+            <p>Próximos eventos</p>
+            <a href="#">Acessar</a>
+          </div>
+
+          <div className="indicator-card card-6">
+            <h3>Dúvidas</h3>
+            <p>5</p>
+            <a href="#">Ver todas</a>
+          </div>
+
+          <div className="vertical-card">
+            <h3>Feedback</h3>
+            <p>Sugestões e mais</p>
+          </div>
+        </div>
       </div>
-        
-        <div className="second-row">
-          <Card title="Perguntas Respondidas" value="40" linkText="Ver Perguntas" className="card-4" />
-          <Card title="Calendário" value="Jan 2024" linkText="Ver Calendário" className="card-5" />
-        </div>
-
-        <div className="third-row">
-          <Card title="Dúvidas" value="Em análise" linkText="Ver Dúvidas" className="card-6" />
-        </div>
-
-        <div className="fourth-row">
-          <Card title="Editar Perfil" value="" linkText="Editar" className="card-7" />
-          <Card title="Adicionar Filhos" value="" linkText="Adicionar" className="card-8" />
-        </div>
-
-        <div className="feedback">
-          <div className="feedback-card">Feedback</div>
-        </div>
-      </div>
-    </>
+    </div>
   );
 }
